9df535ed66f65e05d68c177eb0db022a3c0fb466
William <william.debortoli@hotmail.com>
2026-01-31 20:06:35 -0300

    fix: build fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { Toaster } from '@/components/ui/sonner.tsx'
 import { AuthInitializer } from '@/components/auth/AuthInitializer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    {/* <App /> */}
     <AuthInitializer />
   </StrictMode>,
 )
